ca0a80ea8c6ea1b98d21acd16251412407950d66
Sander Huijsen <sander.huijsen@gmail.com>
2017-10-01 18:25:05 +0800

    A working first stab at an API.

--- server.js
+++ server.js
@@ -1,28 +1,53 @@
-const express = require('express');
-const path = require('path');
+const _ = require('underscore');
+const bearerToken = require('express-bearer-token');
 const bodyParser = require('body-parser');
-const mongodb = require('mongodb');
+const Enigma = require('enigma-sim');
+const express = require('express');
 
-const ObjectID = mongodb.ObjectID;
-const MongoClient = mongodb.MongoClient;
+const BEARER_TOKEN = 'p07ajlbxo7z55v3';
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json);
+app.use(bearerToken());
+app.use(bodyParser.json());
 
-let db;
+const handleError = (res, reason, message, code) => {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+};
 
-MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
-    if (err) {
-        console.log(err);
-        process.exit(1);
+app.post('/v1/encode', (req, res) => {
+    if (req.token !== BEARER_TOKEN) {
+        return handleError(res, 'Authorisation Failed', 'Header does not contain a Bearer Token', 401);
     }
 
-    db = database;
-    console.log('Database connection ready');
+    const body = req.body;
 
-    const server = app.listen(process.env.PORT || 8080, () => {
-        const port = server.address().port;
-        console.log(`App now running on port ${port}`);
-    });
+    if (_.isEmpty(body)) {
+        return handleError(res, 'Missing Information', 'Body contains no useful information', 400);
+    }
+    if (_.isEmpty(body.settings)) {
+        return handleError(res, 'Missing Information', 'You must provide settings', 400);
+    }
+    if (_.isEmpty(body.message)) {
+        return handleError(res, 'Missing Information', 'There is no message to encode', 400);
+    }
+
+    try {
+        const enigma = new Enigma(body.settings);
+        const result = enigma.onMessage(body.message);
+        const response = {
+            message: body.message,
+            encodedMessage: result,
+        };
+        res.status(200).json(response);
+    } catch (e) {
+        return handleError(res, '', e.message, e.code);
+    }
+});
+
+// Initialize the app.
+const server = app.listen(process.env.PORT || 8080, function () {
+    const port = server.address().port;
+    console.log("App now running on port", port);
 });
